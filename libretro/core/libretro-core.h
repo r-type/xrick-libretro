bbbbdf7518fbb727641973c54396ae0de47a6430
twinaphex <libretro@gmail.com>
2017-03-24 03:47:22 +0100

    Cleanups

--- libretro/core/libretro-core.h
+++ libretro/core/libretro-core.h
@@ -11,21 +11,17 @@
 
 #include <stdbool.h>
 
-#define UINT16 uint16_t
-#define UINT32 uint32_t
-
-//#define RENDER16B
 #ifdef  RENDER16B
-	extern uint16_t Retro_Screen[1600*1200];
-	#define PIXEL_BYTES 1
-	#define PIXEL_TYPE UINT16
+extern uint16_t Retro_Screen[1600*1200];
+#define PIXEL_BYTES 1
+#define PIXEL_TYPE uint16_t
 #else
-	extern unsigned int Retro_Screen[1600*1200];
-	#define PIXEL_BYTES 2
-	#define PIXEL_TYPE UINT32
+extern unsigned int Retro_Screen[1600*1200];
+#define PIXEL_BYTES 2
+#define PIXEL_TYPE uint32_t 
 #endif 
 
-#define WINDOW_WIDTH 320
+#define WINDOW_WIDTH  320
 #define WINDOW_HEIGHT 240
 
 #include "libco/libco.h"
@@ -41,21 +37,6 @@ extern int pauseg;
 #include "SDL_video.h"
 
 #define LOGI printf
-
-#define NPLGN 10
-#define NLIGN 5
-#define NLETT 5
-
-#define XSIDE  (CROP_WIDTH/NPLGN -1)
-#define YSIDE  (CROP_HEIGHT/8 -1)
-
-#define YBASE0 (CROP_HEIGHT - NLIGN*YSIDE -8)
-#define XBASE0 0+4+2
-#define XBASE3 0
-#define YBASE3 YBASE0 -4
-
-#define STAT_DECX 120
-#define STAT_YSZ  20
 
 #define RGB565(r, g, b)  (((r) << (5+6)) | ((g) << 6) | (b))
 
